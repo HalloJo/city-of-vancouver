d0caf676a6c848e9a5a384ea956a04883d1974f4
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-05-13 11:46:29 +0200

    Convert basic setup to JSX and styled components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { GlobalStyle } from './styles';
+
+const App = () => {
+  return (
+      <>
+      <GlobalStyle />
+      <div className="App">
+        <h1>City of Vancouver coming through!</h1>
+      </div>
+      </>
+  );
+}
+
+export default App;
